b5e13e5054ad28bfb4d518c5a4760115b6cf7f01
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2026-04-14 16:48:20 -0400

    Add '000691' to excluded Dandiset IDs (#50)
    * Add '000691' to excluded Dandiset IDs
    
    * Apply suggestion from @CodyCBakerPhD

--- scripts/excluded_dandisets.py
+++ scripts/excluded_dandisets.py
@@ -1,2 +1,2 @@
 # Dandiset IDs to exclude from all dashboard processing (e.g., unsupported modalities or test datasets)
-EXCLUDED_DANDISET_IDS = ["000019", "000029", "000144", "000149"]
+EXCLUDED_DANDISET_IDS = ["000019", "000029", "000144", "000149", "000691", "000717", "001188", "001248", "001366", "001424"]
